0a3713c98002abe333ade0fbab5bccb5c4d0848e
MalikChenti <malik.abu6889@gmail.com>
2022-10-19 01:26:07 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,19 @@
-#include "main.h"
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		char s[] = "_putchar";
-			int a;
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
-				a = 0;
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_tabble(int n);
+void num(int r, int n);
 
-					while (a <= 7)
-							{
-										_putchar(s[a]);
-												a++;
-													}
-						_putchar('\n');
-							return (0);
-}
+#endif
